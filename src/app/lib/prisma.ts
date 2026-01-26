eba88d91008fe3b92ec6d2518d46bf82d57e6388
Juvénal5 <edoukoumarc7@gmail.com>
2026-01-26 02:32:07 +0100

    modifie

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,15 +1,18 @@
-import  { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
- 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
- 
-if (process.env.NODE_ENV !== 'production') {
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["error"],
+  });
+
+if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-// Exporter le client Prisma pour utilisation dans l'application
-export default prisma;
+
 
 
